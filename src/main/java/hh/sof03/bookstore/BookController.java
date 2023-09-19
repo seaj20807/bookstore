a6aa6346c4ffadc7497b8a43537588988ef73fec
Sampo Jaakkola <sampo.jaakkola@outlook.com>
2023-09-19 17:27:20 +0300

    Added book listing

--- src/main/java/hh/sof03/bookstore/BookController.java
+++ src/main/java/hh/sof03/bookstore/BookController.java
@@ -1,14 +1,25 @@
 package hh.sof03.bookstore;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BookController {
 
+    @Autowired
+    private BookRepository repository;
+
     @RequestMapping("/index")
     public String index() {
         return "index"; // index.html
+    }
+
+    @RequestMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist"; // booklist.html
     }
 
 }
