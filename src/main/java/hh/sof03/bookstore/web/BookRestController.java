05ad7d3aa54cb2391847c88f064ad8989fdcd9d3
Sampo Jaakkola <sampo.jaakkola@outlook.com>
2023-10-02 20:27:14 +0300

    Added some REST functions (POST and DELETE)

--- src/main/java/hh/sof03/bookstore/web/BookRestController.java
+++ src/main/java/hh/sof03/bookstore/web/BookRestController.java
@@ -5,11 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import hh.sof03.bookstore.domain.Book;
@@ -32,6 +33,18 @@ public class BookRestController {
     @GetMapping("/books/{bookId}")
     public @ResponseBody Optional<Book> findBookRest(@PathVariable("bookId") Long bookId) {
         return bookRepository.findById(bookId);
+    }
+
+    // Tallennetaan uusi kirja, REST
+    @PostMapping("/books")
+    public @ResponseBody Book saveBookRest(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    // Poistetaan kirja, REST
+    @DeleteMapping("/books/{bookId}")
+    public void deleteBookRest(@PathVariable("bookId") Long bookId) {
+        bookRepository.deleteById(bookId);
     }
 
 }
